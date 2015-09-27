f4e26b476ee6e064d7e3aeb2be4de78c89583772
soricellia <soricellia@gmail.com>
2015-09-27 01:50:27 -0400

    cool

--- app/src/main/java/hackny/clarifyproject/MainActivity.java
+++ app/src/main/java/hackny/clarifyproject/MainActivity.java
@@ -1,13 +1,24 @@
 package hackny.clarifyproject;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v4.os.AsyncTaskCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
 import com.clarifai.api.*;
 
 import java.io.File;
@@ -24,6 +35,8 @@ public class MainActivity extends ActionBarActivity {
     final String CLIENT_SECRET = "7yRmlnqGQ-Fz4v-UewzFZYizeZON6G7yFGcPgtyj";
     final String FILEPICKER_KEY = "AtPxVCa4QTxi82DXFa3ZSz";
     final String I_HEART_HACK_NY = "i <3 hackNY";
+    Context context = this;
+    TextView view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -80,8 +93,10 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
     public class ClarifyTask extends AsyncTask<String, Integer, Integer>{
 
+        List<String> tagNames = new ArrayList<>();
         /**
          * Override this method to perform a computation on a background thread. The
          * specified parameters are the parameters passed to {@link #execute}
@@ -104,9 +119,68 @@ public class MainActivity extends ActionBarActivity {
                     clarifai.recognize(new RecognitionRequest(params[0]));
 
             for (Tag tag : results.get(0).getTags()) {
-                Log.v("test 123",tag.getName() + ": " + tag.getProbability());
+                tagNames.add(tag.getName());
+                //Log.v("test 123",tag.getName() + ": " + tag.getProbability());
             }
             return null;
+        }
+
+        /**
+         * <p>Runs on the UI thread after {@link #doInBackground}. The
+         * specified result is the value returned by {@link #doInBackground}.</p>
+         * <p/>
+         * <p>This method won't be invoked if the task was cancelled.</p>
+         *
+         * @param integer The result of the operation computed by {@link #doInBackground}.
+         * @see #onPreExecute
+         * @see #doInBackground
+         * @see #onCancelled(Object)
+         */
+        @Override
+        protected void onPostExecute(Integer integer) {
+            super.onPostExecute(integer);
+
+            ArrayAdapter<String> tagArrayAdapter =
+                    new TagArrayAdapter(context, 0, tagNames);
+            ListView listView = (ListView) findViewById(android.R.id.list);
+            listView.setAdapter(tagArrayAdapter);
+
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    String tagName = tagNames.get(position);
+                }
+            });
+
+        }
+
+    }
+    class TagArrayAdapter extends ArrayAdapter<String> {
+
+        Context context;
+        List<String> tags;
+
+        public TagArrayAdapter(Context context, int resource, List<String> tagNames) {
+            super(context, resource, tagNames);
+
+            this.context = context;
+            this.tags = tagNames;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            String tag = tags.get(position);
+
+            LayoutInflater inflater =
+                    (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+
+            View view = inflater.inflate(R.layout.tag_layout, null);
+
+            TextView tv = (TextView) view.findViewById(R.id.tag);
+            tv.setText(tag);
+
+            return view;
         }
     }
 }
